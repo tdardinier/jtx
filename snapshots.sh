ebc20daca8725486ac8bd5bc7899ff239b11557c
fandejuni <t.dardinier@gmail.com>
2017-06-18 12:04:28 +0200

    Changed snapshots

--- snapshots.sh
+++ snapshots.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 for filename in *.mp4 *.mov *.avi; do
     echo "$filename"
-    ffmpeg -y -ss 00:00:00 -i "$filename" -vframes 1 -filter:v scale="400:-1" "$filename.png"
-    ffmpeg -y -ss 00:00:01 -i "$filename" -vframes 1 -filter:v scale="400:-1" "$filename.png"
-    ffmpeg -y -ss 00:00:05 -i "$filename" -vframes 1 -filter:v scale="400:-1" "$filename.png"
-    ffmpeg -y -ss 00:00:15 -i "$filename" -vframes 1 -filter:v scale="400:-1" "$filename.png"
-    ffmpeg -y -ss 00:00:25 -i "$filename" -vframes 1 -filter:v scale="400:-1" "$filename.png"
+    ffmpeg -y -ss 00:01:00 -i "$filename" -vframes 1 -filter:v scale="320:180" "$filename.png"
 done
-rm snapshots.sh
