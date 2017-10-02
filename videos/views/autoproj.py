d0fa9368693d4d4c0829ae3055ae75c3a2a0a5cc
respoInfo <t.dardinier@gmail.com>
2017-10-02 21:36:00 +0200

    OK

--- videos/views/autoproj.py
+++ videos/views/autoproj.py
@@ -12,7 +12,7 @@ def probe(vid_file_path):
     '''
     if type(vid_file_path) != str:
         # raise Exception('Give ffprobe a full file path of the video')
-        return 0
+        return 1.
 
     command = ["ffprobe",
             "-loglevel",  "quiet",
@@ -44,7 +44,7 @@ def duration(vid_file_path):
     # if everything didn't happen,
     # we got here because no single 'return' in the above happen.
     # raise Exception('I found no duration')
-    return 0
+    return 1.
     #return None
 
 from django.shortcuts import render, get_object_or_404
